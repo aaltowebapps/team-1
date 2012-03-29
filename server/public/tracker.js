b5a8771f2e0cd2ebd5f8f6a69e82ef10e0fea270
Jukka Lahtinen <jukka.lahtinen@aalto.fi>
2012-03-29 17:09:51 +0300

    First 'working' version of GPS tracker =)

--- server/public/tracker.js
+++ server/public/tracker.js
@@ -1,7 +1,37 @@
+'use strict';
 
-function helloWorld() {
-	'use strict';
+var $debugCoordsList = null;
+var coordinateStorage = [];
+var trackerInterval = null;
+var intervalTime = 1000;
 
-	return "Hello world!";
+var tracker = {
+	setDebugCoordsList: function (element) {
+		$debugCoordsList = element;
+	},
+
+	coordinatesToList: function (coordinates) {
+		if (coordinates && coordinates.coords) {
+			if ($debugCoordsList) {
+				$debugCoordsList.append('<div>Lat: ' + coordinates.coords.latitude + ', Lng: '
+					+ coordinates.coords.longitude + ', Accuracy: '+coordinates.coords.accuracy+'</div>');
+			}
+		}
+	},
+
+	pollCoordinates: function () {
+		navigator.geolocation.getCurrentPosition(tracker.coordinatesToList);
+	},
+
+	toggleTracking: function () {
+		if (trackerInterval != null) {
+			clearInterval(trackerInterval);
+			trackerInterval = null;
+		}
+		else {
+			trackerInterval = setInterval('tracker.pollCoordinates()', intervalTime);
+		}
+	}
 }
 
+window.tracker = tracker;
