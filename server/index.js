1f3bbe590d19080dae2b23058d37712171e0a3f4
Amco Dubel <amco.dubel@gmail.com>
2012-04-01 20:29:44 +0300

    Add a dummy 'track' rest API.

--- server/index.js
+++ server/index.js
@@ -2,25 +2,27 @@
 // * node.js
 // * npm install express
 
+/*
 // The server configuration
 var configuration = {
 	PORT: 8080, 
 	ROOT: __dirname + "/public"
 	};
+	*/
 
 // Required modules
 var express = require('express');
+var config = require('./config');
+var track = require('./track');
 
 // Create the server
 var app = express.createServer();
 
-// Serve the REST API
-app.get('/hello/world', function(req, res){
-	res.send('Hello World');
-});
+// Register the 'track' REST API
+track.registerApi(app);
 
 // Serve static files from 'SERVER_ROOT'
-app.use(express.static(configuration.ROOT));
+app.use(express.static(config.ROOT));
 
 // Start the server
-app.listen(configuration.PORT);
+app.listen(config.PORT);
