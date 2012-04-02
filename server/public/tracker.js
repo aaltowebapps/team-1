ca2323545df2c1dcfb620b9c7488c75a64543c23
Jukka Lahtinen <jukka.lahtinen@aalto.fi>
2012-04-02 20:34:22 +0300

    Google Maps working

--- server/public/tracker.js
+++ server/public/tracker.js
@@ -3,10 +3,21 @@
 
 	var $debugCoordsList = null;
 	var coordinateStorage = [];
-	var	trackerInterval = null;
+	var trackerInterval = null;
+	var googleMap = null;
+	var routeLine = null;
 	var lastTimeStamp = 0;
 	var intervalTime = 1000;
 	var tracker = {
+		initializeGoogleMaps: function (domId, centerLat, centerLng) {
+			var myOptions = {
+				center: new google.maps.LatLng(centerLat, centerLng),
+				zoom: 10,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			googleMap = new google.maps.Map(document.getElementById(domId), myOptions);
+		},
+
 		setDebugCoordsList: function (element) {
 			$debugCoordsList = element;
 		},
@@ -14,8 +25,31 @@
 		coordinatesToList: function (coordinates) {
 			if (coordinates && coordinates.coords && coordinates.timestamp) {
 				if ($debugCoordsList) {
-					$debugCoordsList.append('<div>Lat: ' + coordinates.coords.latitude + ', Lng: ' + coordinates.coords.longitude + ', Accuracy: ' + coordinates.coords.accuracy + ', timestamp: ' + coordinates.timestamp + '</div>');
+					var children = $debugCoordsList.children();
+					if (children.length >= 10) {
+						children.last().detach();
+					}
+					$debugCoordsList.prepend('<div>Lat: ' + coordinates.coords.latitude + ', Lng: ' + coordinates.coords.longitude + ', Accuracy: ' + coordinates.coords.accuracy + ', timestamp: ' + coordinates.timestamp + '</div>');
 				}
+				coordinateStorage.push(coordinates);
+			}
+		},
+
+		drawRoute: function () {
+			if (googleMap) {
+				if (!routeLine) {
+					routeLine = new google.maps.Polyline({
+						strokeColor: "#FF0000",
+						strokeOpacity: 1.0,
+						strokeWeight: 2
+					});
+					routeLine.setMap(googleMap);
+				}
+				var arr = [];
+				$.each(coordinateStorage, function (index, item) {
+					arr.push(new google.maps.LatLng(item.coords.latitude, item.coords.longitude));
+				});
+				routeLine.setPath(arr);
 			}
 		},
 
@@ -36,4 +70,4 @@
 	};
 
 	window.tracker = tracker;
-}());
+} ());
