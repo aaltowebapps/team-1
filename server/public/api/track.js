f81c78e0cbc16305bbdd9898dbf1b296481917ee
Amco Dubel <amco.dubel@gmail.com>
2012-04-12 21:59:50 +0300

    Add an initial prototype object for tracks.

---
+++ server/public/api/track.js
@@ -0,0 +1,44 @@
+// Create a Track that uses the GpsApi to access the GPS
+Track = function (gpsApi, xmlHttpRequest) {
+    'use strict';
+
+    // Add protection for calling the constructor without 'new'
+    if (!(this instanceof Track)) { return new Track(gpsApi, xmlHttpRequest); }
+
+    this.gpsApi = gpsApi;
+    this.xmlHttpRequest = xmlHttpRequest;
+    this.serverUrl = "http://localhost/";
+
+    return this;
+};
+
+// Start a new track
+Track.prototype.start = function () {
+    'use strict';
+
+    // This requires the GpsApi
+    if (!this.gpsApi) { return; }
+
+    this.gpsApi.reset();
+    this.gpsApi.start();
+};
+
+// Store the track in the backend
+Track.prototype.save = function (name) {
+    'use strict';
+
+    // This requires the GpsApi
+    if (!this.gpsApi) { return; }
+
+    this.gpsApi.stop();
+
+    var data = this.gpsApi.toBase64();
+
+    // Send the track to the backend
+    this.xmlHttpRequest.open("POST", this.serverUrl, false);
+    this.xmlHttpRequest.setRequestHeader("Content-type", "application/json");
+    this.xmlHttpRequest.setRequestHeader("Content-Length", data.length);
+    this.xmlHttpRequest.send(data);
+
+    this.gpsApi.reset();
+};
