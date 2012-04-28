dad0d7d3a623c3c320bac705a9b3b30ab1f359ea
Amco Dubel <amco.dubel@gmail.com>
2012-04-28 14:35:23 +0300

    Simplify GpsApi unit tests by removing loads of duplicate code (new GeolocationMock).

--- server/public/selftest/unittest/api/gps.js
+++ server/public/selftest/unittest/api/gps.js
@@ -1,16 +1,60 @@
-// A mock geolocation for testing
-var geolocationMock = {
-    watchOptions: undefined,
-    positionCallback: undefined,
-    watcherId: 0,
-    watchPosition: function (positionCallback, positionErrorCallback, watchOptions) {
-        this.positionCallback = positionCallback;
-        this.watchOptions = watchOptions;
-        return this.watcherId;
-    },
-    clearWatch: function (watcherId) {
-        this.watcherId = watcherId;
+//////////////////////////////////////////////////////////////
+// Create a mock geolocation Api
+GeolocationMock = function () {
+    'use strict';
+
+    // Add protection for calling the constructor without 'new'
+    if (!(this instanceof GeolocationMock)) { return new GeolocationMock(); }
+
+    this.positionCallback = undefined;
+    this.positionErrorCallback = undefined;
+    this.watchOptions = undefined;
+    this.watcherId = 123;
+    this.timerId = 0;
+
+    var s = function(lat, lng, alt, spd, dir) {
+        return {
+            "timestamp": 0,
+            "coords": { "latitude": lat, "longitude": lng, 
+                "altitude": alt, "speed": spd, "heading": dir }
+        };
     }
+
+    this.sampleIndex = 0;
+    this.samples = [
+        s(12.345, 67.890, 135, 79, 24),
+        s(54.321,  9.876, 135, 79, 24),
+        s( 1.357,  7.531, 135, 79, 24)
+    ];
+
+    return this;
+};
+
+GeolocationMock.prototype.getCurrentPosition = function (successCallback, errorCallback, options) {
+    this.samples[this.sampleIndex].timestamp = new Date().getTime();
+    successCallback(this.samples[this.sampleIndex]);
+};
+
+GeolocationMock.prototype.watchPosition = function (positionCallback, positionErrorCallback, watchOptions) {
+    this.positionCallback = positionCallback;
+    this.positionErrorCallback = positionErrorCallback;
+    this.watchOptions = watchOptions;
+    return this.watcherId;
+};
+
+GeolocationMock.prototype.clearWatch = function (watcherId) {
+    this.watcherId = watcherId;
+};
+
+GeolocationMock.prototype.nextSample = function () {
+    if (this.sampleIndex >= this.samples.length) { this.sampleIndex = 0; }
+    this.getCurrentPosition(this.positionCallback, this.positionErrorCallback, this.watchOptions);
+    this.sampleIndex = this.sampleIndex + 1;
+};
+
+GeolocationMock.prototype.sample = function (index) {
+    if (index >= this.samples.length) { return; }
+    return this.samples[index];
 };
 
 // A mock geolocation for testing
@@ -25,7 +69,8 @@ var googleMapsMock = {
 
 describe("GpsApi.reset()", function() {
     it("samples removed", function() {
-        var gps = new GpsApi(geolocationMock);
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
         expect(gps.samples).toEqual([]);
         gps.samples = [1,2,3];
         expect(gps.samples).toNotEqual([]);
@@ -34,53 +79,42 @@ describe("GpsApi.reset()", function() {
     });
 
     it("stops existing watch", function() {
-        var gps = new GpsApi(geolocationMock);
-        geolocationMock.watcherId = 123;
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
+        geoMock.watcherId = 123;
         gps.start();
-        expect(geolocationMock.watcherId).toEqual(123);
-        geolocationMock.watcherId = 321;
-        expect(geolocationMock.watcherId).toEqual(321);
+        expect(geoMock.watcherId).toEqual(123);
+        geoMock.watcherId = 321;
+        expect(geoMock.watcherId).toEqual(321);
         gps.reset();
-        expect(geolocationMock.watcherId).toEqual(123);
+        expect(geoMock.watcherId).toEqual(123);
 
     });
 });
 
 describe("GpsApi.start()", function() {
     it("enables high accuracy", function() {
-        var gps = new GpsApi(geolocationMock);
-        geolocationMock.watchOptions = undefined;
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
+        geoMock.watchOptions = undefined;
         gps.start();
-        expect(geolocationMock.watchOptions.enableHighAccuracy).toEqual(true);
+        expect(geoMock.watchOptions.enableHighAccuracy).toEqual(true);
     });
 
     it("stores samples", function() {
-        var gps = new GpsApi(geolocationMock);
-        geolocationMock.positionCallback = undefined;
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
+        geoMock.positionCallback = undefined;
         gps.start();
-        expect(typeof geolocationMock.positionCallback).toEqual("function");
+        expect(typeof geoMock.positionCallback).toEqual("function");
         // Simulate a GPS callback
-        var sample = {
-            "timestamp": 123,
-            "coords": {
-                "latitude": 12.345,
-                "longitude": 67.89,
-                "altitude": 135,
-                "speed": 79,
-                "heading": 24
-            }
-        };
-        geolocationMock.positionCallback(sample);
-        sample.timestamp = 321;
-        sample.coords.latitude = 54.321;
-        sample.coords.longitude = 9.876;
-        geolocationMock.positionCallback(sample);
+        geoMock.nextSample();
+        // And add another sample
+        geoMock.nextSample();
         // Both samples should be stored
         expect(gps.samples.length).toEqual(2);
-        expect(gps.samples[0].ts).toEqual(123);
         expect(gps.samples[0].lat).toEqual(12.345);
         expect(gps.samples[0].lng).toEqual(67.89);
-        expect(gps.samples[1].ts).toEqual(321);
         expect(gps.samples[1].lat).toEqual(54.321);
         expect(gps.samples[1].lng).toEqual(9.876);
     });
@@ -88,40 +122,27 @@ describe("GpsApi.start()", function() {
 
 describe("GpsApi.stop()", function() {
     it("stops correct watcherId", function() {
-        var gps = new GpsApi(geolocationMock);
-        geolocationMock.watcherId = 123;
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
+        geoMock.watcherId = 123;
         gps.start();
-        expect(geolocationMock.watcherId).toEqual(123);
-        geolocationMock.watcherId = 321;
-        expect(geolocationMock.watcherId).toEqual(321);
+        expect(geoMock.watcherId).toEqual(123);
+        geoMock.watcherId = 321;
+        expect(geoMock.watcherId).toEqual(321);
         gps.stop();
-        expect(geolocationMock.watcherId).toEqual(123);
+        expect(geoMock.watcherId).toEqual(123);
     });
 });
 
 describe("GpsApi.toBase64() and GpsApi.fromBase64()", function() {
     it("same samples after a base64 round-trip", function() {
-        var stubStorageObject = {};
-        var gps = new GpsApi(geolocationMock, stubStorageObject);
-        geolocationMock.positionCallback = undefined;
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
         gps.start();
-        expect(typeof geolocationMock.positionCallback).toEqual("function");
         // Simulate a GPS callback
-        var sample = {
-            "timestamp": 123,
-            "coords": {
-                "latitude": 12.345,
-                "longitude": 67.89,
-                "altitude": 135,
-                "speed": 79,
-                "heading": 24
-            }
-        };
-        geolocationMock.positionCallback(sample);
-        sample.timestamp = 321;
-        sample.coords.latitude = 54.321;
-        sample.coords.longitude = 9.876;
-        geolocationMock.positionCallback(sample);
+        geoMock.nextSample();
+        // And add another sample
+        geoMock.nextSample();
 
         var base64data = gps.toBase64();
         gps.reset();
@@ -130,10 +151,8 @@ describe("GpsApi.toBase64() and GpsApi.fromBase64()", function() {
 
         // Both samples should be loaded
         expect(gps.samples.length).toEqual(2);
-        expect(gps.samples[0].ts).toEqual(123);
         expect(gps.samples[0].lat).toEqual(12.345);
         expect(gps.samples[0].lng).toEqual(67.89);
-        expect(gps.samples[1].ts).toEqual(321);
         expect(gps.samples[1].lat).toEqual(54.321);
         expect(gps.samples[1].lng).toEqual(9.876);
     });
@@ -141,53 +160,27 @@ describe("GpsApi.toBase64() and GpsApi.fromBase64()", function() {
 
 describe("GpsApi.status()", function() {
     it("current status is equal to the last sample", function() {
-        var gps = new GpsApi(geolocationMock);
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
         gps.start();
-        // Simulate a GPS callback
-        var sample = {
-            "timestamp": 123,
-            "coords": {
-                "latitude": 12.345,
-                "longitude": 67.89,
-                "altitude": 135,
-                "speed": 79,
-                "heading": 24
-            }
-        };
-        geolocationMock.positionCallback(sample);
-        expect(gps.status()).toEqual(sample.coords);
+        geoMock.nextSample();
         // The sample should be the current status
-        sample.timestamp = 321;
-        sample.coords.latitude = 54.321;
-        sample.coords.longitude = 9.876;
-        geolocationMock.positionCallback(sample);
+        expect(gps.status()).toEqual(geoMock.sample(0).coords);
+        geoMock.nextSample();
         // Now the second sample should be the current status
-        expect(gps.status()).toEqual(sample.coords);
+        expect(gps.status()).toEqual(geoMock.sample(1).coords);
     });
 });
 
 describe("GpsApi.googleMapsTrack()", function() {
     it("converts all samples to 'google.maps.LatLng' type", function() {
-        var gps = new GpsApi(geolocationMock);
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
         gps.start();
         // Simulate a GPS callback
-        var sample = {
-            "timestamp": 123,
-            "coords": {
-                "latitude": 12.345,
-                "longitude": 67.89,
-                "altitude": 135,
-                "speed": 79,
-                "heading": 24
-            }
-        };
-        // Add the sample
-        geolocationMock.positionCallback(sample);
-        sample.timestamp = 321;
-        sample.coords.latitude = 54.321;
-        sample.coords.longitude = 9.876;
+        geoMock.nextSample();
         // Add the second sample
-        geolocationMock.positionCallback(sample);
+        geoMock.nextSample();
         
         googleMapsMock.trace = [];
         var googleFormat = gps.googleMapsTrack(googleMapsMock);
@@ -196,32 +189,17 @@ describe("GpsApi.googleMapsTrack()", function() {
     });
 
     it("convert only new samples to 'google.maps.LatLng' type", function() {
-        var gps = new GpsApi(geolocationMock);
+        var geoMock = new GeolocationMock();
+        var gps = new GpsApi(geoMock);
         gps.start();
         // Simulate a GPS callback
-        var sample = {
-            "timestamp": 123,
-            "coords": {
-                "latitude": 12.345,
-                "longitude": 67.89,
-                "altitude": 135,
-                "speed": 79,
-                "heading": 24
-            }
-        };
-        // Add the sample
-        geolocationMock.positionCallback(sample);
-        sample.timestamp = 321;
-        sample.coords.latitude = 54.321;
-        sample.coords.longitude = 9.876;
+        geoMock.nextSample();
         // Add the second sample
-        geolocationMock.positionCallback(sample);
+        geoMock.nextSample();
+        // Convert the samples to google format, this should be cached
         gps.googleMapsTrack(googleMapsMock);
-        sample.timestamp = 567;
-        sample.coords.latitude = 1.357;
-        sample.coords.longitude = 7.531;
-        // Add the second sample
-        geolocationMock.positionCallback(sample);
+        // Add the third sample
+        geoMock.nextSample();
         
         googleMapsMock.trace = [];
         var googleFormat = gps.googleMapsTrack(googleMapsMock);
